9da75a6aa2bf4a29029009604eb89f6d8ac210b9
vovkahimik <vovkahimik@gmail.com>
2019-12-23 23:21:12 +0300

    tegs + Header form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./Tegs/Header";
+import Main from "./Tegs/Main";
+import Skills from "./Tegs/Skills";
+import Projects from "./Tegs/Projects";
+import Slogan from "./Tegs/Slogan";
+import Contacts from "./Tegs/Contacts";
+import Footer from "./Tegs/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          portfolio <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Main/>
+            <Skills/>
+            <Projects/>
+            <Slogan/>
+            <Contacts/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Tegs/Slogan.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Slogan () {
+    return (
+        <div className="Slogan">
+
+        </div>
+    );
+}
+
+export default Slogan;
