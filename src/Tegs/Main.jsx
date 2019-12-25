28703be2ff5c9a581b820489d27c0d407813b08f
vovkahimik <vovkahimik@gmail.com>
2019-12-25 19:14:32 +0300

    main blocks

--- src/Tegs/Main.jsx
+++ src/Tegs/Main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import styles from './Main.module.css'
 
-function Main () {
+function Main() {
     return (
         <div className={styles.main}>
-
+            <div className={styles.container}>
+                <div className={styles.greeting}>
+                    <span>Меня зовут</span>
+                    <span>Вовка</span>
+                    <span>Химик</span>
+                    <span>+100500 к карме вашей компании</span>
+                </div>
+                <div className={styles.photo}>
+                    <img src="../myPhoto.JPG" alt="не смог добавить фото :("/>
+                </div>
+            </div>
         </div>
     );
 }
