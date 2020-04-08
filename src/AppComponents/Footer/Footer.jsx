c67783a5454dbca35a9fc70acaf06541cefd2668
vovkahimik <vovkahimik@gmail.com>
2020-04-08 23:13:00 +0300

    refactor footer ч.1

--- src/AppComponents/Footer/Footer.jsx
+++ src/AppComponents/Footer/Footer.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import s from './Footer.module.css'
+import styles from './Footer.module.css'
 
-function Footer () {
+function Footer() {
     return (
-        <div className={s.Footer}>
-
+        <div className={styles.footer}>
+            <div className={styles.container}>
+                    <span>Vovka Himik
+                    </span>
+                <div className={styles.net}>
+                    <div className={styles.item}>VK</div>
+                    <div className={styles.item}>FB</div>
+                    <div className={styles.item}>TG</div>
+                    <div className={styles.item}>''</div>
+                </div>
+                <span>© 2020 Все права запрещены
+                </span>
+            </div>
         </div>
     );
 }
