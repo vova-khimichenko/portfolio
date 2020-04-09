b8f819cc8d4d328003d28c438c1e9bdef815a275
vovkahimik <vovkahimik@gmail.com>
2020-04-09 19:42:02 +0300

    Updates

---
+++ precache-manifest.e2eea474845f533f4893e05987f9ee5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f347309f2fae8a2e62813876ddcac8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ea4f5e11c2d5caee80b7",
+    "url": "/portfolio/static/css/main.b4ac272d.chunk.css"
+  },
+  {
+    "revision": "3b3c88e24a9471462b04",
+    "url": "/portfolio/static/js/2.2fcf90f1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.2fcf90f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ea4f5e11c2d5caee80b7",
+    "url": "/portfolio/static/js/main.aeff5a2a.chunk.js"
+  },
+  {
+    "revision": "8575fab3f3395c90d845",
+    "url": "/portfolio/static/js/runtime-main.680679b4.js"
+  }
+]);
