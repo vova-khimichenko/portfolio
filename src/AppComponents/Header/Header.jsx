45a9695369c70a12b7f1eb5a212d4ba2db65f0d5
vovkahimik <vovkahimik@gmail.com>
2021-01-21 18:57:46 +0300

    animation links in header

--- src/AppComponents/Header/Header.jsx
+++ src/AppComponents/Header/Header.jsx
@@ -5,21 +5,16 @@ import avaPhoto from '../../assets/images/myPhoto.jpg'
 function Header() {
     const year = new Date().getFullYear()
     return (
-        <div className={styles.container}>
+        <div className={styles.header}>
             <div className={styles.photoBlock}>
                 <img className={styles.photo} src={avaPhoto} alt={'avaPhoto'}/>
             </div>
             <nav className={styles.navMenu}>
-                <a href='/home'
-                   className={styles.link}>Home</a>
-                <a href='/about'
-                   className={styles.link}>About</a>
-                <a href='/skills'
-                   className={styles.link}>Skills</a>
-                <a href='/projects'
-                   className={styles.link}>Projects</a>
-                <a href='/contacts'
-                   className={styles.link}>Contacts</a>
+                <a href='/home' className={styles.link}>Home</a>
+                <a href='/about' className={styles.link}>About</a>
+                <a href='/skills' className={styles.link}>Skills</a>
+                <a href='/projects' className={styles.link}>Projects</a>
+                <a href='/contacts' className={styles.link}>Contacts</a>
             </nav>
             <div className={styles.copyright}>
                 © {year} <span className={styles.userName}>vovkahimik</span>
