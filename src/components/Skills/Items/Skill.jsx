0f526b14587a56757c8283b459e79fcd9c4856d8
vovkahimik <vovkahimik@gmail.com>
2021-02-05 13:08:45 +0300

    icon faTags in title skills

--- src/components/Skills/Items/Skill.jsx
+++ src/components/Skills/Items/Skill.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './Skill.module.scss'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faTag, faTags} from "@fortawesome/free-solid-svg-icons";
 
 function Skill(props) {
     return (
         <div className={styles.skillContainer}>
-            <h6 className={styles.title}>{props.title}</h6>
+            <h6 className={styles.title}>{props.title} <FontAwesomeIcon icon={faTags}/></h6>
             <div className={styles.backgroundImage}>
                 <div className={styles.content}>
                     {props.content}
