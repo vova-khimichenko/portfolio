ed9665a62d2f2a3dfa87d80a11fabea5d988fcb1
vovkahimik <vovkahimik@gmail.com>
2021-02-18 23:10:36 +0300

    business cards for contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import styles from './Contact.module.scss'
+import {faMobileAlt} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 function Contact() {
     return (
         <div className={styles.page}>
             <div className={styles.titleBlock}>
-                <h2 className={styles.title}>Contact me</h2>
-                <span className={styles.titleShadow}>Contact me</span>
+                <h2 className={styles.title}>Call me</h2>
+                <span className={styles.titleShadow}>Call me</span>
             </div>
-            {/*<form className={styles.tegForm}>*/}
-            {/*    <input className={styles.tegInput}/>*/}
-            {/*    <input className={styles.tegInput}/>*/}
-            {/*    <textarea className={styles.tegTextarea}/>*/}
-            {/*</form>*/}
-            {/*<button className={styles.button}*/}
-            {/*        type="submit">Отправить*/}
-            {/*</button>*/}
-            {/*</div>*/}
+            <div className={styles.businessCards}>
+                <span className={styles.card}></span>
+                <span className={styles.card}></span>
+                <span className={styles.card}></span>
+                <span className={styles.card}></span>
+                <span className={styles.card}>
+                    <FontAwesomeIcon className={styles.icon} icon={faMobileAlt}/>   <a href={'+375295169670'}>+375-29-516-96-70</a>
+
+                </span>
+            </div>
         </div>
     );
 }
