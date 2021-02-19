dbba01a35406b28786d50779d5997980a81ceca1
vovkahimik <vovkahimik@gmail.com>
2021-02-19 13:36:59 +0300

    keep cards

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './Contact.module.scss'
 import {faMobileAlt} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import react from '../../assets/images/icons8-react-native-64.png'
 
 function Contact() {
     return (
@@ -10,14 +11,24 @@ function Contact() {
                 <h2 className={styles.title}>Call me</h2>
                 <span className={styles.titleShadow}>Call me</span>
             </div>
-            <div className={styles.businessCards}>
-                <span className={styles.card}></span>
-                <span className={styles.card}></span>
-                <span className={styles.card}></span>
-                <span className={styles.card}></span>
-                <span className={styles.card}>
-                    <FontAwesomeIcon className={styles.icon} icon={faMobileAlt}/>   <a href={'+375295169670'}>+375-29-516-96-70</a>
-
+            <div className={styles.cards}>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span>
+                    <div className={styles.about}>
+                        <div className={styles.icons}>
+                            <h4>Vladimir</h4><h4>Khimichenko</h4>
+                            <h5>frontend developer</h5>
+                        </div>
+                        <img src={react}/>
+                    </div>
+                    <div className={styles.phone}>
+                        <a href={'+375295169670'}>+375-29-516-96-70</a>
+                        <FontAwesomeIcon icon={faMobileAlt} style={{margin: '0 14px'}}/>
+                        <a href={'+375295169670'}>+375-29-516-96-70</a>
+                    </div>
                 </span>
             </div>
         </div>
