9ce1d251f94b6eeda0473ac7af7ffbf04e9b99d4
vovkahimik <vovkahimik@gmail.com>
2021-02-20 18:44:46 +0300

    adaptation contact page for mobile view

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,6 +5,21 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import react from '../../assets/images/icons8-react-native-64.png'
 
 function Contact() {
+
+    const card = <span>
+                    <div className={styles.about}>
+                        <div className={styles.icons}>
+                            <h4>Vladimir</h4><h4>Khimichenko</h4>
+                            <h5>frontend developer</h5>
+                        </div>
+                        <img src={react} alt={'reactIcon'}/>
+                    </div>
+                    <div className={styles.phone}>
+                        <FontAwesomeIcon icon={faMobileAlt} style={{margin: '0 14px'}}/>
+                        <a href={'+375295169670'}>+375-29-516-96-70</a>
+                    </div>
+                </span>
+
     return (
         <div className={styles.page}>
             <div className={styles.titleBlock}>
@@ -14,22 +29,13 @@ function Contact() {
             <div className={styles.cards}>
                 <span></span>
                 <span></span>
+                {card}
                 <span></span>
                 <span></span>
-                <span>
-                    <div className={styles.about}>
-                        <div className={styles.icons}>
-                            <h4>Vladimir</h4><h4>Khimichenko</h4>
-                            <h5>frontend developer</h5>
-                        </div>
-                        <img src={react}/>
-                    </div>
-                    <div className={styles.phone}>
-                        <a href={'+375295169670'}>+375-29-516-96-70</a>
-                        <FontAwesomeIcon icon={faMobileAlt} style={{margin: '0 14px'}}/>
-                        <a href={'+375295169670'}>+375-29-516-96-70</a>
-                    </div>
-                </span>
+                {card}
+                {card}
+                {card}
+                {card}
             </div>
         </div>
     );
